45701aeafd0124a3ff144946d1133f54c49c7821
nobuwobu <carolina97ho@gmail.com>
2025-04-26 12:53:07 -0300

    Update CarritoTotal.jsx

--- Switch/src/components/CarritoTotal.jsx
+++ Switch/src/components/CarritoTotal.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const CarritoTotal = () => {
-    const { moneda, delivery_fee, getCarritoAmount } = useContext(ShopContext);
+    const { moneda, delivery_fee, getCarritoCantidad } = useContext(ShopContext);
 
     return (
         <div className='w-full'>
             <div className='flex flex-col gap-2 mt-2 text-sm'>
                 <div className='flex justify-between'>
                     <p>Subtotal</p>
-                    <p>{moneda} {getCarritoAmount()}.00</p>
+                    <p>{moneda} {getCarritoCantidad()}.00</p>
                 </div>
                 <hr />
                 <div className='flex justify-between'>
@@ -19,7 +19,7 @@ const CarritoTotal = () => {
                 <hr />
                 <div className='flex justify-between'>
                     <b>Total</b>
-                    <b>{moneda} {getCarritoAmount() === 0 ? 0 : getCarritoAmount() + delivery_fee}.00</b>
+                    <b>{moneda} {getCarritoCantidad() === 0 ? 0 : getCarritoCantidad() + delivery_fee}.00</b>
                 </div>
             </div>
         </div>
