bfd34319157aee571c1b0e83e23b3ba83b0536d1
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2025-04-29 10:59:44 -0300

    Actualizacion del diseño de quienes somos
    Cambios en el diseño en quienes somos

--- Switch/src/pages/quienessomos/quienessomos.jsx
+++ Switch/src/pages/quienessomos/quienessomos.jsx
@@ -6,22 +6,22 @@ export function QuienesSomosPage() {
         <div className="container-quienessomos">
             <div className="content">
                 <h1 className="quienessomos">¿Quiénes Somos?</h1>
-                <p className="info">Un equipo apasionado por innovar y crear soluciones que mejoren el mundo. Descubre al equipo que está detrás de esta visión.</p>
+                <p className="info">Un equipo apasionado por innovar y crear soluciones que mejoren el mundo Descubre al equipo que está detrás de esta visión</p>
                 <div className="team-cards">
                     <div className="team-card">
-                        <div className="team-img"/>
+                        <img src="src/assets/Tomas.jpg" alt="Team Member" className="team-img"/>
                         <h3 className="team-name">Tomas Vazquez Brouver</h3>
                         <p className="team-role">Fundador</p>
                     </div>
                     <div className="team-card">
-                        <div className="team-img" />
+                    <img src="src/assets/.jpg" alt="Team Member" className="team-img"/>
                         <h3 className="team-name">Joaquin Cardozo</h3>
                         <p className="team-role">Cofundador</p>
                     </div>
                     <div className="team-card">
-                        <div className="team-img" />
+                    <img src="src/assets/.jpg" alt="Team Member" className="team-img"/>
                         <h3 className="team-name">Carolina Belen Ho</h3>
-                        <p className="team-role">Diseñadora</p>
+                        <p className="team-role">Programadora y Diseñadora</p>
                     </div>
                 </div>
             </div>
