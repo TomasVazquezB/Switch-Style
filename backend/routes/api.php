2d3d0989e0d85ffc3b1f83303e07a4e5a171bea6
CJoako4 <joaquin.cardozo@davinci.edu.ar>
2025-05-12 00:54:59 -0300

    conexión exitosa a la base de datos
    y adición de datos a la base de datos como verificación de testeo.

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+
+Route::get('/producto', [ProductoController::class, 'index']);
+Route::get('/test', function () {
+    return response()->json(['message' => 'API funcionando correctamente']);
+});
