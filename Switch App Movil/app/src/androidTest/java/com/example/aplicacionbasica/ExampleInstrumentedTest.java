0af0216d5c767ae5bec21501f4d04cf168540931
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2025-05-16 12:15:53 -0300

    Cambios en el proyecto

---
+++ Switch App Movil/app/src/androidTest/java/com/example/aplicacionbasica/ExampleInstrumentedTest.java
@@ -0,0 +1,19 @@
+package com.example.aplicacionbasica;
+
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.aplicacionbasica", appContext.getPackageName());
+    }
+}
