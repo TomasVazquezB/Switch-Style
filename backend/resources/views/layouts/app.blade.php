d48f07a6989b32ba7e4474a6a0ee67c047f5d9a3
nobuwobu <carolina97ho@gmail.com>
2025-06-07 20:12:16 -0300

    Fixed Tailwind para Create Ropa

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -1,2 +1,11 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi App</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-50 text-gray-800">
+    @yield('content')
+</body>
+</html>
