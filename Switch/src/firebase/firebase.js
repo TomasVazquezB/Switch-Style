51470d36341e7221b806fc2fbe88ac99523525c4
CJoako4 <joaquin.cardozo@davinci.edu.ar>
2025-06-09 02:24:19 -0300

    actualización de SDK

--- Switch/src/firebase/firebase.js
+++ Switch/src/firebase/firebase.js
@@ -1,5 +1,8 @@
 // src/firebase/firebase.js
+// firebase.js
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "TU_API_KEY",
@@ -10,7 +13,8 @@ const firebaseConfig = {
   appId: "APP_ID"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
