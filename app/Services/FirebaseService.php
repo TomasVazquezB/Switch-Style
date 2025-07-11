21cb271ed01f5b039b4fd83f2ca3b7c5b38236be
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2025-07-11 10:59:56 -0300

    Actualizacion

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -2,33 +2,19 @@
 
 namespace App\Services;
 
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth;
 use Google\Cloud\Firestore\FirestoreClient;
 
 class FirebaseService
 {
-    protected Auth $auth;
     protected FirestoreClient $firestore;
 
     public function __construct()
     {
-        $firebaseCredentials = config('firebase.credentials.file');
-
-        if (!file_exists($firebaseCredentials)) {
-            throw new \Exception("Archivo de credenciales Firebase no encontrado en: $firebaseCredentials");
-        }
-
-        $factory = (new Factory)
-            ->withServiceAccount($firebaseCredentials);
-
-        $this->auth = $factory->createAuth();
-        $this->firestore = $factory->createFirestore()->database();
-    }
-
-    public function getAuth(): Auth
-    {
-        return $this->auth;
+        $this->firestore = new FirestoreClient([
+            'projectId' => env('FIREBASE_PROJECT_ID'),
+            'keyFilePath' => config('firebase.credentials.file'),
+            'transport' => env('FIRESTORE_TRANSPORT', 'grpc'), 
+        ]);
     }
 
     public function getFirestore(): FirestoreClient
