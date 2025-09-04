ca98b46126c104cfd647d647ba55dabdc0ad9d5f
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2025-09-04 10:51:41 -0300

    Actualizacion

--- Switch/src/pages/quienessomos/quienessomos.jsx
+++ Switch/src/pages/quienessomos/quienessomos.jsx
@@ -17,12 +17,12 @@ export function QuienesSomosPage({ darkMode }) {
         <p className="info"style={{ color: darkMode ? "#ffffffff" : "#000000ff" }}>Somos un equipo apasionado por innovar y crear soluciones que mejoren el mundo. Descubre al equipo que está detrás de esta visión</p>
         <div className="team-cards">
           <div className="team-card">
-            <img src="src/assets/Tomas.jpg" alt="Tomas Vazquez Brouver" className="team-img" />
+            <img src="https://res.cloudinary.com/switchstyle/image/upload/v1756993701/Tomas_p9fkoc.jpg" alt="Tomas Vazquez Brouver" className="team-img" />
             <h3 className="team-name"style={{ color: darkMode ? "#ffffff" : "#111111" }}>Tomas Vazquez Brouver</h3>
             <p className="team-role">Fundador</p>
           </div>
           <div className="team-card">
-            <img src="src/assets/Joaquin.jpg" alt="Joaquin Cardozo" className="team-img" />
+            <img src="https://res.cloudinary.com/switchstyle/image/upload/v1756993761/Joaquin_clsh8b.jpg" alt="Joaquin Cardozo" className="team-img" />
             <h3 className="team-name"style={{ color: darkMode ? "#ffffff" : "#111111" }}>Joaquin Cardozo</h3>
             <p className="team-role">Cofundador</p>
           </div>
