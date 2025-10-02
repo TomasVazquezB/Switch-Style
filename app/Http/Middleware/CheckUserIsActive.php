3d971bec4b09261a22785ab14f3f54dc5d17b7c0
nobuwobu <carolina97ho@gmail.com>
2025-10-01 22:32:45 -0300

    Revert "User change"
    This reverts commit 5fa79251675ce05053288427d48077aa07af51a3.

--- app/Http/Middleware/CheckUserIsActive.php
+++ app/Http/Middleware/CheckUserIsActive.php
@@ -4,21 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class CheckUserIsActive
 {
     public function handle(Request $request, Closure $next)
     {
-        // Si no hay usuario autenticado, que lo resuelva 'auth'
-        if (!$request->user()) {
-            return redirect()->route('login');
-        }
+        $user = $request->user();
 
-        if (!$request->user()->is_active) {
-            Auth::logout();
+        if ($user && !$user->is_active) {
+            auth()->logout();
             return redirect()->route('login')
-                ->withErrors(['login' => 'Cuenta deshabilitada por un administrador.']);
+                ->withErrors(['Cuenta deshabilitada por un administrador.']);
         }
 
         return $next($request);
