19ae5fd07609640b37acdeddb50b3e9a0ee97e2d
nobuwobu <carolina97ho@gmail.com>
2025-10-03 00:15:44 -0300

    Eliminacion de firebase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,33 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth as FirebaseAuth;
-use Kreait\Firebase\Contract\Firestore;
+
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(FirebaseAuth::class, function () {
-            $serviceAccountPath = app()->environment('production') ? '/tmp/firebase_credentials.json': base_path('firebase_credentials.json');
-
-            $factory = (new Factory)->withServiceAccount($serviceAccountPath);
-            return $factory->createAuth();
-        });
-
-        $this->app->singleton(Firestore::class, function () {$serviceAccountPath = app()->environment('production') ? '/tmp/firebase_credentials.json': base_path('firebase_credentials.json');
-
-            return (new Factory)
-                ->withServiceAccount($serviceAccountPath)
-                ->createFirestore();
-        });
+        if (!config('services.firebase.enabled')) {
+            return;
+        }
     }
 
     public function boot(): void
     {
-        if (app()->environment('production')) {$json = env('FIREBASE_CREDENTIALS_JSON');
-            if ($json && !file_exists('/tmp/firebase_credentials.json')) {file_put_contents('/tmp/firebase_credentials.json', $json);}
+        if (!config('services.firebase.enabled')) {
+            return;
         }
+
     }
 }
