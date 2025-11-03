0da5111e595e890595c258ca5914bb0cb3dc50fc
Tomas Vazquez Brouver <131373718+TomasVazquezB@users.noreply.github.com>
2025-11-03 10:22:20 -0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        'api/mobile/*',
         'api/*', // Exime todas las rutas que empiecen con /api/
         'sanctum/csrf-cookie',
         'api/login',
