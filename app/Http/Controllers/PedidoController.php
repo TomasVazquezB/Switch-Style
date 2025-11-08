5a1f25d85d924c4d3fc35b9468033b678adef9dc
CJoako4 <joaquin.cardozo@davinci.edu.ar>
2025-11-08 19:39:33 -0300

    actualización

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pedido;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log; 
 
 class PedidoController extends Controller
 {
