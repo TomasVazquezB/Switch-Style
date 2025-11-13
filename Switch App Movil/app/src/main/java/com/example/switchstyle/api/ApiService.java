4fefd42883e393bf09838869c91e10bb63fd34c8
Tomas Vazquez Brouver <vbtomas@hotmail.com>
2025-11-13 10:18:54 -0300

    Actualizacion Switch Style

--- Switch App Movil/app/src/main/java/com/example/switchstyle/api/ApiService.java
+++ Switch App Movil/app/src/main/java/com/example/switchstyle/api/ApiService.java
@@ -3,7 +3,6 @@ package com.example.switchstyle.api;
 import com.google.gson.JsonObject;
 import java.util.List;
 import java.util.Map;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -11,24 +10,22 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ApiService {
+
     @POST("api/mobile/login")
     Call<AuthResponse> login(@Body LoginRequest request);
 
     @POST("api/mobile/register")
     Call<AuthResponse> register(@Body RegisterRequest request);
 
-    @GET("api/productos")
-    Call<List<Product>> getProductos(@Header("Authorization") String authHeader);
-
-    @GET("api/productos")
-    Call<ResponseBody> getProductosRaw(@Header("Authorization") String token);
-
+    // 🔹 Ropa (sin token)
     @GET("api/ropa")
     Call<List<Product>> getRopa();
 
+    // 🔹 Accesorios (sin token)
     @GET("api/accesorios")
     Call<List<Product>> getAccesorios();
 
+    // 🔹 Sincronizar favoritos
     @POST("api/favoritos/update")
     Call<JsonObject> updateFavoritos(
             @Header("Authorization") String token,
